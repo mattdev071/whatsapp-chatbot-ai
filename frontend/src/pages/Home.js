15a07bfcb6b814387e8577b3a3e969ef3a4f153c
mattdev071 <mattdev071@gmail.com>
2025-03-30 01:44:38 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,9 +3,6 @@ import FlowEditor from "../components/FlowEditter/FlowEditor";
 import LeftBar from "../components/LeftBar/LeftBar";
 import "../pages/Home.css";
 
-const FLOW_ID = "67e82de939b64bf75627f4ad"
-const BUSINESS_ID = "67e81d1ca90b3e881fb6b2d5"
-
 export default function Home() {
   return (
     <div className="home-container">
